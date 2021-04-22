7f63d84f4f0a0e29b3b165c20675ecc703dd33b9
MARCIO RIOS KUNIMURA <marcio_rios_kunimura@hotmail.com>
2021-04-22 15:21:29 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoHorario/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoHorario.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
